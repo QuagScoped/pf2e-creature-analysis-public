28acdd1d18ff4faa4df5428a1167859b01075bbe
QuagScoped <alexanderhoogerbrug@gmail.com>
2024-02-01 00:52:28 +0100

    Create main_script.py

---
+++ main_script.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 00:49:48 2024
+
+@author: Alexander
+"""
+
+def main():
+    print("This is the main program")
+
+if __name__ == '__main__':
+    main()
