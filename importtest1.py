4436798b53c1c86f6aec7db02434bcf446b6617f
QuagScoped <alexanderhoogerbrug@gmail.com>
2024-02-01 01:10:09 +0100

    uploading datasets and starting writing main.py

---
+++ importtest1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 00:58:46 2024
+
+@author: Alexander
+"""
+
+def testfunction():
+    print("test successful!")
--- main_script.py
+++ main_script.py
@@ -5,8 +5,11 @@ Created on Thu Feb  1 00:49:48 2024
 @author: Alexander
 """
 
+from importtest1 import testfunction
+
 def main():
     print("This is the main program")
+    testfunction()
 
 if __name__ == '__main__':
     main()
