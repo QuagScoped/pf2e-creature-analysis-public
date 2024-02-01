e67c2f3bc75ef7f80fbfbf80b7ba966f97c5affb
QuagScoped <alexanderhoogerbrug@gmail.com>
2024-02-01 02:22:40 +0100

    first working version

--- functions.py
+++ functions.py
@@ -7,5 +7,15 @@ Created on Thu Feb  1 01:25:51 2024
 This is a library file, you should not be here.
 """
 
-def plot_perlevel():
-    print("Hello world.")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_per_level(creatures_dataset, stat, correction_data):
+    
+    # taking mean per level and subtracting moderate values
+    mean_per_level = creatures_dataset.groupby("Level")[stat].mean().reset_index()
+    merged_filter = pd.merge(mean_per_level, correction_data[["Level", "Moderate"]], on="Level", how="left")
+    merged_filter[stat] -= merged_filter["Moderate"]
+    
+    plt.plot(merged_filter["Level"], merged_filter[stat], ".", label=f"Mean {stat} of filtered creatures.", markersize=14)
+
--- main_script.py
+++ main_script.py
@@ -16,13 +16,23 @@ path = r"C:\Users\Alexander\Desktop\Code Projects\pf2e-creature-analysis-public"
 sys.path.append(path)
 
 # function imports
-from functions import *
+from functions import plot_per_level
 
 # library imports
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # global file imports
 creature_stats = pd.read_excel("./datasets/all_books_creatures.xlsx")
+
+saves_by_level = pd.read_excel("./datasets/saves_by_level.xlsx")
+saves_by_level_rel = pd.read_excel("./datasets/saves_by_level_rel.xlsx")
+
+ac_by_level = pd.read_excel("./datasets/ac_by_level.xlsx")
+ac_by_level_rel = pd.read_excel("./datasets/ac_by_level_rel.xlsx")
+    
+mods_by_level = pd.read_excel("./datasets/abilities_by_level.xlsx")
+mods_by_level_rel = pd.read_excel("./datasets/abilities_by_level_rel.xlsx")
 
 def filter_by_key(*filters):
     '''
@@ -30,37 +40,57 @@ def filter_by_key(*filters):
     ----------
     *filters : the keywords (traits/languages/source/immunities/sizes) of the creatures you want to keep
     format as ("keyword type", keyword1), ("keyword type", keyword2)
+    valid keywords: Traits, Languages, Source, Size, Immunities, Resistances, Weaknesses (and technically Name)
 
     Returns
     -------
     the list of creatures matching the traits you want to filter by
     '''
-    return creature_stats[
-        any(
-            creature_stats[column].apply(lambda x: term in str(x))
-            for column, term in filters
-        )
+    conditions = [
+        creature_stats[column].apply(lambda x: term in str(x))
+        for column, term in filters
     ]
+    return creature_stats[(pd.concat(conditions, axis=1).sum(axis=1) > 0)]
+
+def filter_out_key(*filters):
+    '''
+    Parameters
+    ----------
+    *filters : the keywords (traits/languages/source/immunities/sizes) of the creatures you want to remove
+    format as ("keyword type", keyword1), ("keyword type", keyword2)
+    valid keywords: Traits, Languages, Source, Size, Immunities, Resistances, Weaknesses (and technically Name)
+
+    Returns
+    -------
+    the list of creatures without the traits you want to filter by
+    '''
+    conditions = [
+        creature_stats[column].apply(lambda x: term in str(x))
+        for column, term in filters
+    ]
+    return creature_stats[~(pd.concat(conditions, axis=1).any(axis=1))]
+
+def plot_settings():
+    print("Hello world.")
+    plt.grid()
+    # plt.savefig("./testfig.png", dpi=250)
+    plt.show()
 
 def main():
-    
-    # local file imports
-    saves_by_level = pd.read_excel("./datasets/saves_by_level.xlsx")
-    saves_by_level_rel = pd.read_excel("./datasets/saves_by_level_rel.xlsx")
 
-    ac_by_level = pd.read_excel("./datasets/ac_by_level.xlsx")
-    ac_by_level_rel = pd.read_excel("./datasets/ac_by_level_rel.xlsx")
+    # filter data here
+    keywords = (("Immunities", "mental"),
+                ("Traits", "swarm"),
+                ("Traits", "mindless")
+        )
+   
+    filtered_creatures = filter_by_key(*keywords)
     
-    mods_by_level = pd.read_excel("./datasets/abilities_by_level.xlsx")
-    mods_by_level_rel = pd.read_excel("./datasets/abilities_by_level_rel.xlsx")
-   
-   # filter data here 
-   
     # calling functions
+    plot_per_level(filtered_creatures, "Will", saves_by_level)
+        
+    # showing the plot, look in the function if you want to change any settings
+    plot_settings()
     
-    plot_perlevel()
-    
-    print("This is the main program")
-
 if __name__ == '__main__':
     main()
